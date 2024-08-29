f80921975f972d32004b3de4d0dad36c485b6658
gandharrdotexe <gandharbagde@gmail.com>
2024-08-30 00:41:48 +0530

    nabar done

--- project-ui/tailwind.config.js
+++ project-ui/tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'], // Scans all JS/JSX/TS/TSX files in the src directory
+  content: ['./src/**/*.{js,jsx,ts,tsx}',"./node_modules/flowbite/**/*.js"], // Scans all JS/JSX/TS/TSX files in the src directory
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+],
 };
