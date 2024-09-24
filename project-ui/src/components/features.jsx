3d782c8187a3566ef8500ac38cb575e10fbba913
gandharrdotexe <gandharbagde@gmail.com>
2024-09-25 00:23:23 +0530

    features.jsx done responsive

--- project-ui/src/components/features.jsx
+++ project-ui/src/components/features.jsx
@@ -39,16 +39,16 @@ const Features = () => {
     }, []);
 
     return (
-        <section className='py-20'>
+        <section className='py-20 md:py-24 flex flex-col items-center justify-center'>
             <div className="container">
-                <h2 className='text-5xl font-medium text-center tracking-tighter'>Elevate your SEO efforts.</h2>
-                <p className='text-white/70 text-lg tracking-tight text-center mt-5'>
+                <h2 className='text-5xl md:text-6xl font-medium text-center tracking-tighter'>Elevate your SEO efforts.</h2>
+                <p className='text-white/70 text-lg md:text-xl max-w-2xl mx-auto tracking-tight text-center mt-5 py-2'>
                     From small startups to large enterprises,
                     our AI-driven tool has revolutionized the way businesses approach SEO.
                 </p>
-                <div className='mt-10'>
+                <div className='mt-10 flex flex-col lg:flex-row gap-3'>
                 {tabs.map((tab) => (
-                    <div key={tab.title} className='border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center m-3'>
+                    <div key={tab.title} className='border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center m-3 lg:flex-1'>
                         <div className='h-12 w-12 border border-white/15 rounded-lg inline-flex items-center justify-center'>
                             {/* Using the dotlottie-player directly */}
                             <dotlottie-player
@@ -68,7 +68,15 @@ const Features = () => {
                     
                 ))}
                 </div>
-                <img src={ProductImage} alt="productImage" />
+                <div className='border border-white/20 p-2.5 rounded-xl mt-3 mx-2.5'>
+                <div className='aspect-video bg-cover border border-white/20 p-2.5' 
+                    style={{
+                        backgroundImage: `url(${ProductImage})`
+                    }}
+                ></div>
+                {/* <img src={ProductImage} alt="productImage" /> */}
+                </div>
+                
             </div>
         </section>
     );
