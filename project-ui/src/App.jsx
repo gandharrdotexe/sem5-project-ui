cf423f58a979d86c0ce08d7793e2fdc0994ff56c
gandharrdotexe <gandharbagde@gmail.com>
2024-09-26 23:37:19 +0530

    working on waitlist

--- project-ui/src/App.jsx
+++ project-ui/src/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Navbar from './components/navbar'; // Make sure the path is correct
 import Design from './components/3ddesign';
 import Features from './components/features';
-import SimpleAnimation from './components/simpleanimation';
-import waitList from './components/waitList';
+import WaitList from './components/waitList'
 
 function App() {
   return (
@@ -13,7 +11,7 @@ function App() {
       <Navbar />
       <Design />
       <Features />
-      <waitList />
+      <WaitList />
 
     </div>
   );
--- project-ui/src/components/waitList.jsx
+++ project-ui/src/components/waitList.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const waitList = () =>{
     return (
         <section className='py-20'>
-            <div className='container'>
+            <div className='container mx-auto px-4'>
                 <h2>AI-driven SEO for everyone</h2>
                 <p>Achieve clear, impactful results without the complexity.</p>
                 <button className='relative py-3 px-4 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a2058] shadow-[0_0_12px_#8c45ff]'>
@@ -13,7 +13,7 @@ const waitList = () =>{
                                 <div className='rounded-lg border absolute inset-0 border-white/40 [mask-image:linear-gradient(to_top, black, transparent)]'></div>
                                 <div className='absolute inset-0 shadow-[0_0_10px_rgba(140,60,255,0.7)_inset] rounded-lg'></div>
                             </div>
-                            <span className='font-semibold text-lg'>Get Started</span>
+                            <span className='font-semibold text-lg'>Join WaitList</span>
                 </button>
             </div>
             
