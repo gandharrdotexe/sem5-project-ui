f001f55d7a1e09720b5f7418944662af3f94ef7e
gandharrdotexe <gandharbagde@gmail.com>
2024-10-01 23:32:15 +0530

    designing footer

--- project-ui/src/components/footer.jsx
+++ project-ui/src/components/footer.jsx
@@ -1,8 +1,25 @@
 import React from "react";
-
+import LogoIcon from '../assets/logo.svg';
 const Footer = () =>{
     return (
-        <footer></footer>
+        <footer>
+            <div>
+                <div>
+                    <img src={LogoIcon} alt="Logo" className='h-8 w-8 invert' /> 
+                    <div>AI-SEO Tool</div>
+                </div>
+                <div>
+                    <nav>
+                        <a href=""></a>
+                        <a href=""></a>
+                        <a href=""></a>
+                        <a href=""></a>
+                        <a href=""></a>
+                    </nav>
+                </div>
+                <div></div>
+            </div>
+        </footer>
     );
 };
 
