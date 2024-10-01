5579f414609326a069d243049c378fd1f6d035e1
gandharrdotexe <gandharbagde@gmail.com>
2024-10-01 23:54:46 +0530

    changes in desktop view earlier code

--- project-ui/src/components/footer.jsx
+++ project-ui/src/components/footer.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import LogoIcon from '../assets/logo.svg';
+import InstaLogo from '../assets/social-instagram.svg';
+import XLogo from '../assets/social-x.svg';
+import YTLogo from '../assets/social-youtube.svg';
 const Footer = () =>{
     return (
-        <footer>
-            <div>
+        <footer className="py-5 border-t border-white/15">
+            <div className="container">
+                <div className="flex flex-col">
                 <div>
                     <img src={LogoIcon} alt="Logo" className='h-8 w-8 invert' /> 
                     <div>AI-SEO Tool</div>
@@ -17,7 +21,14 @@ const Footer = () =>{
                         <a href="">Blogs</a>
                     </nav>
                 </div>
-                <div></div>
+                <div>
+                    <img src={InstaLogo} alt="Logo" className='h-8 w-8 invert' /> 
+                    <img src={XLogo} alt="Logo" className='h-8 w-8 invert' /> 
+                    <img src={YTLogo} alt="Logo" className='h-8 w-8 invert' /> 
+
+                </div>
+                </div>
+                
             </div>
         </footer>
     );
