ebe93b7de2232aa8356e950d49155fd0bfe76ed0
gandharrdotexe <gandharbagde@gmail.com>
2024-10-22 19:11:52 +0530

    repaired css

--- project-ui/src/components/get-started.jsx
+++ project-ui/src/components/get-started.jsx
@@ -63,32 +63,37 @@ const Get_Started = () => {
     <div>
       <Navbar />
       <section
-        className='h-[492px] md:h-[800px] flex justify-center items-center overflow-hidden relative -mt-[100px]'
+        className={`flex justify-center items-center relative -mt-[100px] transition-all duration-500 ${
+          data ? 'h-auto' : 'h-[492px] md:h-[800px]'
+        }`}
         style={{
           backgroundImage: `url(${Stars})`,
-          maskImage: 'linear-gradient(to bottom, transparent, black 10%, black 90%, transparent)',
+          
         }}
       >
         <div className="container">
-          <div className='flex items-center justify-center gap-2'>
-            <input
-              type="text"
-              className='py-2 px-3 w-80 border border-white/40 caret-[#9867C5]'
-              onChange={handleChange}
-              placeholder="Enter URL"
-            />
-            <button
-              className='relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a2058] shadow-[0_0_12px_#8c45ff] flex items-center space-x-2'
-              onClick={analyze}
-            >
-              <div className='absolute inset-0'>
-                <div className='rounded-lg border border-white/20 absolute inset-0 [mask-image:linear-gradient(to_bottom, black, transparent)]'></div>
-                <div className='rounded-lg border absolute inset-0 border-white/40 [mask-image:linear-gradient(to_top, black, transparent)]'></div>
-                <div className='absolute inset-0 shadow-[0_0_10px_rgb(140,60,255,.7)_inset] rounded-lg'></div>
-              </div>
-              <span>Analyze</span>
-              <img src={LogoIcon} alt="Logo" className='h-4 w-4 invert' />
-            </button>
+          <div className='flex items-center justify-center gap-2 mt-[115px]'>
+            
+              <input
+                type="text"
+                className='py-2 px-3 w-[50%] border border-white/40 caret-[#9867C5]'
+                onChange={handleChange}
+                placeholder="Enter URL"
+              />
+              <button
+                className='relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a2058] shadow-[0_0_12px_#8c45ff] flex items-center space-x-2'
+                onClick={analyze}
+              >
+                <div className='absolute inset-0'>
+                  <div className='rounded-lg border border-white/20 absolute inset-0 [mask-image:linear-gradient(to_bottom, black, transparent)]'></div>
+                  <div className='rounded-lg border absolute inset-0 border-white/40 [mask-image:linear-gradient(to_top, black, transparent)]'></div>
+                  <div className='absolute inset-0 shadow-[0_0_10px_rgb(140,60,255,.7)_inset] rounded-lg'></div>
+                </div>
+                <span>Analyze</span>
+                <img src={LogoIcon} alt="Logo" className='h-4 w-4 invert' />
+              </button>
+            
+            
           </div>
 
           {/* Display the loading, error, or SEO analysis result */}
@@ -97,7 +102,7 @@ const Get_Started = () => {
             {error && <p>{error}</p>}
             {data && (
   <>
-    <div>
+    <div className='my-11'>
       <SEODataTable seoData={data} />  {/* Passing the fetched data to the Table component */}
       <button
                     className='relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a2058] shadow-[0_0_12px_#8c45ff] flex items-center space-x-2'
